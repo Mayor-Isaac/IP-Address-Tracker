f25655af5c42464c21aeb5b5847f51c2c4ce6aea
mayor-isaac <ogunyilekaferanmi@gmail.com>
2023-05-04 03:59:15 +0100

    M-04

--- app.js
+++ app.js
@@ -1,64 +1,99 @@
-'use strict'
+"use strict";
 
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-        function(position) {
-            // console.log(position);
+// VARIABLES
+const input = document.querySelector(".input input");
+const search = document.querySelector(".send");
+const ipAddress = document.querySelector(".ip-address");
+const $timezone = document.querySelector(".timezone");
+const $country = document.querySelector(".country");
+const $city = document.querySelector(".city");
+const code = document.querySelector(".postalCode");
+const $isp = document.querySelector(".isp");
 
-            const { latitude, longitude } = position.coords;
-
-            console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
-
-            const coords = [latitude, longitude];
-
-            const map = L.map('map').setView(coords, 10)
-
-            console.log(map);
-
-            console.log(L);
-
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-              attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          }).addTo(map);
-
-          const marker = L.marker(coords).addTo(map)
-
-          marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup()
-
-        //   API
-        const renderErr = function(errMsg){
-            const errDiv = document.createElement('div')
-            errDiv.classList.add('error')
-            errDiv.innerHTML =`${errMsg}`
-            document.querySelector('.top-container').append(errDiv)
-        }
-
-        const getaddressPro = function(para){
-            // fetch(`http://ip-api.com/json/192.212.174.101`)
-            fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_hUdp8MaSA3JQDttLmCj2iQQvRlu7G&ipAddress=8.8.8.8`)
-            .then(function (response) {
-              if(!response.ok){
-                throw new Error('Invalid IP address or domain')
-              } 
-              return response.json();
-            })
-            .then(function(data){
-                console.log(data);
-                // ipAddress.textContent = data.ip
-                // region.textContent = data.location.region
-                // asn.textContent = data.as.asn
-                // country.textContent = data.location.country
-                // timezone.textContent = data.location.timezone
-                // isp.textContent = data.isp
-            }).catch(err =>{
-              renderErr('Invalid IP address or domain')
-            })
-        }
-        // getaddressPro('197.210.85.177/32')
-        getaddressPro()
-          },
-      function () {
-        alert('Please allow browser to see your location');
-      } /* fail callback */
-    ); 
+const getPosition = function(){
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+         loadMap ,
+        function () {
+          alert('Please allow browser to see your location');
+        } /* fail callback */
+      );
+    }
   }
+
+const renderErr = function (errMsg) {
+  const errDiv = document.createElement("div");
+  errDiv.classList.add("error");
+  errDiv.innerHTML = `${errMsg}`;
+  document.querySelector(".top-container").append(errDiv);
+};
+
+const loadMap = function (position) {
+  console.log(position);
+
+  const { latitude, longitude } = position.coords;
+
+  const coords = [latitude, longitude];
+
+  const map = L.map("map").setView(coords, 15);
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+
+  const marker = L.marker(coords).addTo(map);
+  
+};
+
+const getaddressPro = function (para) {
+
+  fetch(
+    `https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_hUdp8MaSA3JQDttLmCj2iQQvRlu7G&ipAddress=${para}`
+  )
+    .then(function (response) {
+      if (!response.ok) {
+        throw new Error("Invalid IP address or domain");
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      const { ip, isp, location } = data;
+      const { city, country, timezone, lat, lng, postalCode } = location;
+
+      ipAddress.textContent = ip;
+      $city.textContent = city;
+      code.textContent = postalCode;
+      $country.textContent = country;
+      $timezone.textContent = timezone;
+      $isp.textContent = isp;
+
+      const myPosition = {
+        coords: {
+          latitude: lat,
+          longitude: lng,
+        },
+      };
+      // console.log(myPosition);
+      loadMap(myPosition);
+    })
+    .catch((err) => {
+      renderErr("Invalid IP address or domain");
+    });
+};
+
+
+// getaddressPro('19.117.63.126')
+// getaddressPro('197.210.85.177')
+
+input.focus();
+search.addEventListener("click", function () {
+  getaddressPro(input.value);
+});
+input.addEventListener("keypress", function (e) {
+  if (e.which === 13) {
+    getaddressPro(input.value);
+  }
+});
